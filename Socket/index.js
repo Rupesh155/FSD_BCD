3a996ead1ef719b3e21ab7340ef55c62a695c98d
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-06-22 00:32:46 +0530

    chat app with multiple users

--- Socket/index.js
+++ Socket/index.js
@@ -1,19 +1,93 @@
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+// app.set('view engine','ejs')
+// app.get('/',(req,res)=>{
+//     res.render('index')
+// })
+
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//         // console.log(msg);
+//       io.emit('chat message', msg);
+//     });
+//   });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
+
+// const express = require('express');
+// const { createServer } = require('http');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// const server = createServer(app);
+// const io = new Server(server);
+
+// app.set('view engine','ejs')
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+// io.on('connection', (socket) => {
+//     console.log('user');
+//     socket.on('chat message', (msg) => {
+//     //   console.log('message: ' + msg);
+//     io.emit('chat message', msg);
+
+//     });
+//   });
+
+// server.listen(3000, () => {
+//   console.log('server running at http://localhost:3000');
+// });
+
 const express = require('express');
-const { createServer } = require('node:http');
+const { createServer } = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-app.set('view engine','ejs')
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+let users = {};
+
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        // console.log(msg);
-      io.emit('chat message', msg);
+    console.log('a user connected');
+
+    socket.on('join', (username) => {
+        users[socket.id] = username;
+        socket.broadcast.emit('user connected', username);
+        io.emit('update users', Object.values(users));
     });
-  });
+
+    socket.on('chat message', (msg) => {
+        const messageData = {
+            username: users[socket.id],
+            message: msg
+        };
+        io.emit('chat message', messageData);
+    });
+
+    socket.on('disconnect', () => {
+        const username = users[socket.id];
+        delete users[socket.id];
+        io.emit('user disconnected', username);
+        io.emit('update users', Object.values(users));
+        console.log('user disconnected');
+    });
+});
 
 server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
